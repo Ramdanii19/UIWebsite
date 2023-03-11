852a8e2cd3ec9160e2af4420e7a5750ae84e7bae
Ramdani <110803481+Ramdanii19@users.noreply.github.com>
2023-03-11 17:55:39 +0700

    menambahkan layouts

---
+++ layouts/footer.blade.php
@@ -0,0 +1,27 @@
+<!-- ======= Footer ======= -->
+<div style="position: fixed; left: 0; bottom: 0; width: 100%; text-align: center; background-color: #ffffff;">
+    <footer id="footer" class="footer">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Muhamad Ramdani</span></strong>. All Rights Reserved
+      </div>
+    </footer><!-- End Footer -->
+  </div>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{ asset('UIWebsite/assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
+  <script src="{{ asset('UIWebsite/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{ asset('UIWebsite/assets/vendor/chart.js/chart.umd.js')}}"></script>
+  <script src="{{ asset('UIWebsite/assets/vendor/echarts/echarts.min.js')}}"></script>
+  <script src="{{ asset('UIWebsite/assets/vendor/quill/quill.min.js')}}"></script>
+  <script src="{{ asset('UIWebsite/assets/vendor/simple-datatables/simple-datatables.js')}}"></script>
+  <script src="{{ asset('UIWebsite/assets/vendor/tinymce/tinymce.min.js')}}"></script>
+  <script src="{{ asset('UIWebsite/assets/vendor/php-email-form/validate.js')}}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{ asset('UIWebsite/assets/js/main.js')}}"></script>
+
+</body>
+
+</html>
---
+++ layouts/sidebar.blade.php
@@ -0,0 +1,101 @@
+  <!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="/">
+          <i class="bi bi-grid"></i>
+          <span>Dashboard</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link " href="/">
+          <i class="bi bi-grid"></i>
+          <span>Data Siswa</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="/">
+          <i class="bi bi-grid"></i>
+          <span>Data Petugas</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="/">
+          <i class="bi bi-grid"></i>
+          <span>Data Kelas</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="/">
+          <i class="bi bi-grid"></i>
+          <span>Data SPP</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="/">
+          <i class="bi bi-grid"></i>
+          <span>Pembayaran</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="/">
+          <i class="bi bi-grid"></i>
+          <span>History Pembayaran</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="/">
+          <i class="bi bi-grid"></i>
+          <span>Generate Laporan</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <!-- End Components Nav -->
+
+      <!-- End Forms Nav -->
+
+      <!-- End Tables Nav -->
+
+      <!-- End Charts Nav -->
+
+      <!-- End Icons Nav -->
+
+      <li class="nav-heading">Pages</li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="/profile">
+          <i class="bi bi-person"></i>
+          <span>Profile</span>
+        </a>
+      </li><!-- End Profile Page Nav -->
+
+      <!-- End F.A.Q Page Nav -->
+
+      <!-- End Contact Page Nav -->
+
+      <!-- End Register Page Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="{{ route('logout') }}">
+          <i class="bi bi-box-arrow-in-right"></i>
+          <span>Logout</span>
+        </a>
+      </li>
+      <!-- End Login Page Nav -->
+
+      <!-- End Error 404 Page Nav -->
+
+      <!-- End Blank Page Nav -->
+
+    </ul>
+
+  </aside><!-- End Sidebar-->
